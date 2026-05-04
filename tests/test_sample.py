80afae6deb13e5620e4445057588570c842322cd
Saha, Debadrita <160181147+sahadebadrita@users.noreply.github.com>
2026-05-03 20:46:41 -0400

    modified test,added coverage

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os, sys
+import pytest
 #import qlgates
 
 #folder_path1 = os.path.abspath('../')
@@ -7,9 +8,11 @@ import os, sys
 from qlgates.utils import getRzzgate   # adjust import
 #from utils import getRzzgate   # adjust import
 
-def test_rzz_shape():
-    n = 2
-    U = getRzzgate(n, NQL=2, theta1=0.5)
+@pytest.mark.parametrize("theta1",[0.2,0.4,0.8])
+@pytest.mark.parametrize("n",[2,4,8])
+def test_rzz_shape(n,theta1):
+    #n = 2
+    U = getRzzgate(n, NQL=2, theta1=theta1)
     assert U.shape == (4*n*n, 4*n*n)
 
 def test_rzz_theta_zero_identity():
