2f5b7c375711ac438ce9099d7cc16d233fac4b1f
Saha, Debadrita <160181147+sahadebadrita@users.noreply.github.com>
2026-05-03 22:13:01 -0400

    modified testfile

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,11 +5,27 @@ import pytest
 
 #folder_path1 = os.path.abspath('../')
 #sys.path.append(folder_path1)
-from qlgates.utils import getRzzgate   # adjust import
+from qlgates.utils import getRzzgate, qldit   # adjust import
 #from utils import getRzzgate   # adjust import
 
-@pytest.mark.parametrize("theta1",[0.2,0.4,0.8])
-@pytest.mark.parametrize("n",[2,4,8])
+def test_qldit_shape(small_config):
+
+    R = qldit(
+        n=small_config.n,
+        k=small_config.k,
+        d=small_config.d,
+        l=small_config.l,
+        coupling=small_config.coupling,
+        periodic=small_config.periodic,
+        full=small_config.full,
+    )
+
+    assert R.shape == (small_config.n, small_config.n)
+
+
+
+#@pytest.mark.parametrize("theta1",[0.2,0.4,0.8])
+#@pytest.mark.parametrize("n",[2,4,8])
 def test_rzz_shape(n,theta1):
     #n = 2
     U = getRzzgate(n, NQL=2, theta1=theta1)
