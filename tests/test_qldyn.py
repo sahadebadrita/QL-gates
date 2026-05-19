a755800a33d7786910d7ad2e7c8d5c42236f4b50
Saha, Debadrita <160181147+sahadebadrita@users.noreply.github.com>
2026-05-19 12:34:24 -0400

    added unit tests for hamiltonians

--- tests/test_qldyn.py
+++ tests/test_qldyn.py
@@ -2,7 +2,10 @@ import numpy as np
 import os, sys
 import pytest
 
-from qlgates.qldyn import transverseN, bell_state
+from qlgates.hamiltonians import transverseN
+from qlgates.run_dynamics import bell_state
+from tests.conftest import small_config
+#from tests.conftest import small_config
 
 def test_transverseN(small_config):
     U = transverseN(
@@ -36,3 +39,10 @@ def test_bell_state_shape(cfg, psi0):
     psi = bell_state(cfg, psi0, "psi_minus")
 
     assert psi.shape == psi0.shape
+
+def test_transverseN_is_unitary():
+    """Trotter unitary must be unitary for any parameters."""
+    #cfg = small_config
+    Ug = transverseN(small_config.n, small_config.NQL, small_config.J, small_config.h, small_config.deltat, debug=False)
+    should_be_identity = Ug @ Ug.conj().T
+    assert np.allclose(should_be_identity, np.eye(len(Ug)), atol=1e-10)
