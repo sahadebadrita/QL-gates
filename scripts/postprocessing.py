6a846be95fa2d3f343f61e8a4a76e20133a786b7
Saha, Debadrita <160181147+sahadebadrita@users.noreply.github.com>
2026-06-04 11:03:45 -0400

    visualization scripts modified

--- scripts/postprocessing.py
+++ scripts/postprocessing.py
@@ -40,24 +40,23 @@ def main():
         mz.append(build_observable(cfg,'z',j))
     M_eq = []  # Store equilibrium values for each site
     ratios = []
-    expectation_mz = np.zeros((cfg.timesteps+1, cfg.NQL+2))  # Store expectation values for each site and time step
+    expectation_mz = np.zeros((cfg.timesteps, cfg.NQL+2))  # Store expectation values for each site and time step
     for h in [0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0]:
         # Load final states
         psit = np.load(f"./della_slurm_runs/h{h}_J{cfg.J}_ql_N{cfg.NQL}.npz")['arr_0']
         for j in range(len(mz)):
             expectation_mz[:,j] = expectation(psit, mz[j])
-        expectation_mz[:,-1] = np.arange(cfg.timesteps+1)  # Add time steps as the last column
+        expectation_mz[:,-1] = np.arange(cfg.timesteps)  # Add time steps as the last column
         expectation_mz[:,-2] = np.mean(expectation_mz[:,:-2], axis=1)  # Add mean expectation values as the second-to-last column
         #save expectation values
         np.savez(f"./della_slurm_runs/h{h}_J{cfg.J}_ql_N{cfg.NQL}_expectation.npz",expectation_mz)
-        M_eq.append(np.mean(np.abs(expectation_mz[int(0.8*cfg.timesteps):,-2])))  # Mean of the absolute values of the mean expectation values in the last 20% of time steps
+        M_eq.append(np.mean(np.abs(expectation_mz[int(0.0*cfg.timesteps):,-2])))  # Mean of the absolute values of the mean expectation values in the last 20% of time steps
         ratios.append(abs(h/cfg.J))  # Ratio of the equilibrium value at this h to the equilibrium value at the smallest h
     Meq = np.array(M_eq)
     simpleplot(ratios,Meq,
-    filename=f"./della_slurm_runs/h{h}_J{cfg.J}_ql_N{cfg.NQL}_magnetization.png",
+    filename=f"./della_slurm_runs/T{cfg.timesteps * cfg.deltat}_dt{cfg.deltat}_J{cfg.J}_ql_N{cfg.NQL}_magnetization.png",
     xlabel="h/J",
-    ylabel="Equilibrium Magnetization",
-    title=fr"$\delta t = {cfg.dt},\; T = {cfg.timesteps * cfg.dt}\,\mathrm{fs}$"
-)
+    ylabel="Magnetization (time-averaged)",
+    title=fr"$\delta t = {cfg.deltat},\; T = {cfg.timesteps * cfg.deltat}\,\mathrm{{fs}}$")
 if __name__ == "__main__":
     main()
